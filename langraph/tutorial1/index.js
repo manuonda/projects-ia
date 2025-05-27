19a06c7204fd989aa91d12a979fde29327149d5c
David Garcia <david.garcia@grupomost.com>
2025-05-27 19:28:16 -0300

    Initialize tutorial1 with package.json and basic MessageGraph implementation

---
+++ langraph/tutorial1/index.js
@@ -0,0 +1,42 @@
+import {END, START, MessageGraph} from "@langchain/langgraph"
+import * as fs from "fs"
+
+
+const funcA = input => {
+    input[0].content += "Agent takes action A;";
+    return input
+}
+
+const funcB = input => {
+    input[0].content += "Agent takes action B;"
+    return input
+}
+
+//build the graph 
+const graph = new MessageGraph()
+.addNode("nodeA", funcA)
+.addNode("nodeB", funcB)
+
+//edges
+.addEdge(START, "nodeA")
+.addEdge("nodeA","nodeB")
+.addEdge("nodeB", END);
+
+const runnable = graph.compile()
+const result = await runnable.invoke("Input;")
+console.log(result)
+
+
+// Conditiona Edges in LangGraph 
+const funBuy = input => {
+    console.log("input funBuy , ", input)
+    input[0].content += "= => Agent will buy stocks"
+    return input
+}
+
+const funSell = input => {
+    input[0].content += "==> Agent will sell stocks"
+    return input
+}
+
+const funDecision
