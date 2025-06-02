578d76248521392eaa58a6c26fabcf39a33ca687
david garcia <david.garcia@grupomost.com>
2025-06-01 22:01:50 -0300

    add project

---
+++ spring-boot/spring-ia/src/main/java/com/spring/ia/spring_ia/SpringIaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ia.spring_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIaApplication.class, args);
+	}
+
+}
---
+++ spring-boot/spring-ia/src/test/java/com/spring/ia/spring_ia/SpringIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ia.spring_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
