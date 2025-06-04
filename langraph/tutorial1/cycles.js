b5ad5c559f04ea4d272a40e1b0264371cb93b102
David Garcia <david.garcia@grupomost.com>
2025-06-03 21:38:33 -0300

    Add initial implementation of cycles.js for message graph handling

---
+++ langraph/tutorial1/cycles.js
@@ -0,0 +1,8 @@
+import { START, END, MessageGraph } from "@langchain/langgraph";
+import { HumanMessage } from "@langchain/core/messages";
+
+
+const funAgent = input =>input 
+const funUseDiceTool = input => {
+    const dice = 1+ Math
+}
