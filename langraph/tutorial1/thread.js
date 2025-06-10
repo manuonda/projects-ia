81e85d26e279e6afd66c37ece9a70b30927debd3
david garcia <david.garcia@grupomost.com>
2025-06-10 00:02:29 -0300

    refactor code thread js

--- langraph/tutorial1/thread.js
+++ langraph/tutorial1/thread.js
@@ -4,7 +4,7 @@ import {ChatOpenAI} from "@langchain/openai"
 
 
 import * as dotenv from "dotenv"
-dotenv.load()
+dotenv.config()
 
 const llm = new ChatOpenAI({
     model:"gpt-4o",
@@ -21,4 +21,41 @@ const callModelNode = async(state) => {
 }
 
 const workflow = new StateGraph(MessagesAnnotation)
-.add
+.addNode("agent", callModelNode)
+.addEdge(START, "agent")
+.addEdge("agent",END)
+
+
+// initialize memory to persis state between graph runs
+const checkpointer = new MemorySaver()
+const graph = workflow.compile({checkpointer})
+
+console.log("Starting first thread")
+const configIntroThread = {
+    configurable : { thread_id : "t1"}
+}
+
+//First lets introducer ourserlve to the AI 
+const t1r1 = await graph.invoke({
+    messages: [
+        new HumanMessage("hi! My name is Daniel and I like LangGraph")
+
+    ],
+}, configIntroThread);
+console.log(getLastMessage(t1r1));
+
+//Does the Ai remember us ?
+const t1r2 = await graph.invoke({
+    messages:[
+      new HumanMessage("soyrr, did I already introudce myself")
+    ],
+}, configIntroThread)
+console.log(getLastMessage(t1r2).content)
+
+
+// Yes you did! you mentioned that your name is Daniel 
+// and that you like langgraph
+console.log("Starting second thread")
+const configAnotherThread = {
+    configurable: { thread_id: "t2"}
+}
