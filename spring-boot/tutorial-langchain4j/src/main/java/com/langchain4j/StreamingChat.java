c14b1d96f3fec07bb18e43675098d510ea89b174
david garcia <david.garcia@grupomost.com>
2025-07-22 15:46:28 -0300

    feat: Enhance StreamingChat to support streaming responses with StreamingResponseBody

--- spring-boot/tutorial-langchain4j/src/main/java/com/langchain4j/StreamingChat.java
+++ spring-boot/tutorial-langchain4j/src/main/java/com/langchain4j/StreamingChat.java
@@ -1,10 +1,17 @@
 package com.langchain4j;
 
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import dev.langchain4j.model.chat.StreamingChatModel;
 
@@ -21,11 +28,14 @@ public class StreamingChat {
     @Autowired
     private StreamingChatModel streamingChatModel;
 
-    @PostMapping("/stream")
-    public String postMethodName(@RequestBody ChatDTO entity) {
-        //TODO: process POST request
-        
-        return entity;
+    @PostMapping("/stream", produces= MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<StreamingResponseBody> basicStream(@RequestBody ChatDTO entity) {
+        StreamingResponseBody stream = outputStream -> {
+          try(PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream))){
+
+          }
+        };
+        return null;
     }
     
 }
